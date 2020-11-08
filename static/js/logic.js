ea33bb368012cc47264c36c0ffcb5f87bde5138d
patricialan <54603331+patricialan@users.noreply.github.com>
2020-11-08 15:40:59 -0700

    updated site

--- static/js/logic.js
+++ static/js/logic.js
@@ -31,38 +31,101 @@ let map = L.map("map", {
 L.control.layers(baseMaps).addTo(map);
 
 // Markers
-var redIcon = new L.Icon({
-    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
-    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41]
-  });
+var redMarker1 = L.ExtraMarkers.icon({
+    icon: 'fa-number',
+    markerColor: 'orange-dark',
+    shape:'circle',
+    prefix: 'fa',
+    number: '1'
+});
 
-L.marker([53.601391, -113.372253], {icon: redIcon}).addTo(map)
-  .bindPopup('<h6>CLAREVIEW</h6><b>Stair Count:</b> 111<br><b>Lat, Long:</b> 53.601391, -113.372253<br><b>Street Parking:</b> 13912 20 St NW, Edmonton, AB T5Y 1P8');
+var redMarker2 = L.ExtraMarkers.icon({
+    icon: 'fa-number',
+    markerColor: 'orange-dark',
+    shape:'circle',
+    prefix: 'fa',
+    number: '2'
+});
 
-L.marker([53.558961, -113.442285], {icon: redIcon}).addTo(map)
-  .bindPopup('<h6>CONCORDIA COLLEGE</h6><b>Stair Count:</b> 244<br><b>Lat, Long:</b> 53.558961, -113.442285<br><b>Street Parking:</b> 7040 Ada Blvd NW, Edmonton, Alberta');
+var redMarker3 = L.ExtraMarkers.icon({
+    icon: 'fa-number',
+    markerColor: 'orange-dark',
+    shape:'circle',
+    prefix: 'fa',
+    number: '3'
+});
 
-L.marker([53.534811, -113.468342], {icon: redIcon}).addTo(map)
-  .bindPopup('<h6>CONNORS HILL</h6><b>Lat, Long:</b> 53.534811, -113.468342<br><b>Street parking:</b> 9103 Strathearn Dr NW, Edmonton, Alberta<br><br><img id="myImg" src="https://lh3.ggpht.com/p/AF1QipPUFPFcXJGAPeJo19donSHjeKPOoYGBXM5wDNM3=s1536" onclick="openModal();currentSlide(1)">');
+var redMarker4 = L.ExtraMarkers.icon({
+    icon: 'fa-number',
+    markerColor: 'orange-dark',
+    shape:'circle',
+    prefix: 'fa',
+    number: '4'
+});
 
-L.marker([53.538805, -113.489561], {icon: redIcon}).addTo(map)
-    .bindPopup('<h6>FAIRMONT HOTEL MACDONALD</h6><b>Stair Count:</b> 201-204<br><b>Lat, Long:</b> 53.538805, -113.489561<br><b>Parking Lot:</b> Grierson Hill NW Parking (9769-9939 Grierson Hill NW, Edmonton, AB T5J 1N9)<br>Notes: Parking lot for Louise McKinney Riverfront Park.<br><br><img id="myImg" src="https://lh3.ggpht.com/p/AF1QipM8OXLE3BBQieyqk2Rvk_obHT7BO1o4n6PeFA1b=s1536" onclick="openModal();currentSlide(2)">');
+var redMarker5 = L.ExtraMarkers.icon({
+    icon: 'fa-number',
+    markerColor: 'orange-dark',
+    shape:'circle',
+    prefix: 'fa',
+    number: '5'
+});
 
-L.marker([53.532510, -113.511644], {icon: redIcon}).addTo(map)
-    .bindPopup('<h6>GLENORA EZIO/FARAONE</h6><b>Stair Count:</b> 186-202<br><b>Lat, Long:</b> 53.532510, -113.511644<br><b>Parking Lot:</b> Royal Glenora Club (11160 River Valley Rd NW, Edmonton, AB T5J 2G7)<br><br><img id="myImg" src="https://lh3.ggpht.com/p/AF1QipNg2j_QXBAkb293oMVMYCKK7hnjxIV2KoacEXZ-=s1536" onclick="openModal();currentSlide(3)">');
+var redMarker6 = L.ExtraMarkers.icon({
+    icon: 'fa-number',
+    markerColor: 'orange-dark',
+    shape:'circle',
+    prefix: 'fa',
+    number: '6'
+});
 
-L.marker([53.501849, -113.559412], {icon: redIcon}).addTo(map)
-    .bindPopup('<h6>GRANDVIEW WHITEMUD PARK</h6><b>Stair Count:</b> 242-261<br><b>Lat, Long:</b> 53.501849, -113.559412<br><b>Parking Lot:</b> Alfred H. Savage Centre (13909 Fox Dr NW, Edmonton, AB T6H 4P3)<br><br><img id="myImg" src="https://lh3.ggpht.com/p/AF1QipMueO4d4TXGYfSOwsd6SVKSoQkeeTQ_9RuWXHww=s1536" onclick="openModal();currentSlide(4)">');
+var redMarker7 = L.ExtraMarkers.icon({
+    icon: 'fa-number',
+    markerColor: 'orange-dark',
+    shape:'circle',
+    prefix: 'fa',
+    number: '7'
+});
 
-L.marker([53.508143, -113.566463], {icon: redIcon}).addTo(map)
-    .bindPopup('<h6>LAURIER DRIVE</h6><b>Stair Count:</b> 165<br><b>Lat, Long #1:</b> 53.508143, -113.566463<br><b>Lat, Long #2: </b> 53.508232, -113.563099<br><b>Street parking:</b> 121 Laurier Dr NW, Edmonton, Alberta<br><b>Notes:</b> Two separate staircases with same entrance from street parking.');
+var redMarker8 = L.ExtraMarkers.icon({
+    icon: 'fa-number',
+    markerColor: 'orange-dark',
+    shape:'circle',
+    prefix: 'fa',
+    number: '8'
+});
 
-L.marker([53.499416, -113.592445], {icon: redIcon}).addTo(map)
-    .bindPopup('<h6>WOLF WILLOW</h6><b>Stair Count:</b> 200-201<br><b>Lat, Long:</b> 53.499416, -113.592445<br><b>Street Parking:</b> 162 Wolf Willow Cres, Edmonton, Alberta<br><br><img id="myImg" src="https://lh3.ggpht.com/p/AF1QipNII55xNMEPfgUTKqD4pTAX5cmiizsyMlXJATFm=s1536" onclick="openModal();currentSlide(5)">');
+L.marker([53.601391, -113.372253], {icon: redMarker1}).addTo(map)
+    .bindPopup('<h6>CLAREVIEW</h6><b>Stair Count:</b> 111<br><b>Lat, Long:</b> 53.601391, -113.372253<br><b>Street Parking:</b> 13912 20 St NW, Edmonton, AB T5Y 1P8')
+    .bindTooltip("CLAREVIEW");
+
+L.marker([53.558961, -113.442285], {icon: redMarker2}).addTo(map)
+    .bindPopup('<h6>CONCORDIA COLLEGE</h6><b>Stair Count:</b> 244<br><b>Lat, Long:</b> 53.558961, -113.442285<br><b>Street Parking:</b> 7040 Ada Blvd NW, Edmonton, Alberta')
+    .bindTooltip("CONCORDIA COLLEGE");
+
+L.marker([53.534811, -113.468342], {icon: redMarker3}).addTo(map)
+    .bindPopup('<h6>CONNORS HILL</h6><b>Lat, Long:</b> 53.534811, -113.468342<br><b>Street parking:</b> 9103 Strathearn Dr NW, Edmonton, Alberta<br><br><img id="myImg" src="https://lh3.ggpht.com/p/AF1QipPUFPFcXJGAPeJo19donSHjeKPOoYGBXM5wDNM3=s1536" onclick="openModal();currentSlide(1)">')
+    .bindTooltip("CONNORS HILL");
+
+L.marker([53.538805, -113.489561], {icon: redMarker4}).addTo(map)
+    .bindPopup('<h6>FAIRMONT HOTEL MACDONALD</h6><b>Stair Count:</b> 201-204<br><b>Lat, Long:</b> 53.538805, -113.489561<br><b>Parking Lot:</b> Grierson Hill NW Parking (9769-9939 Grierson Hill NW, Edmonton, AB T5J 1N9)<br>Notes: Parking lot for Louise McKinney Riverfront Park.<br><br><img id="myImg" src="https://lh3.ggpht.com/p/AF1QipM8OXLE3BBQieyqk2Rvk_obHT7BO1o4n6PeFA1b=s1536" onclick="openModal();currentSlide(2)">')
+    .bindTooltip("FAIRMONT HOTEL MACDONALD");
+
+L.marker([53.532510, -113.511644], {icon: redMarker5}).addTo(map)
+    .bindPopup('<h6>GLENORA EZIO/FARAONE</h6><b>Stair Count:</b> 186-202<br><b>Lat, Long:</b> 53.532510, -113.511644<br><b>Parking Lot:</b> Royal Glenora Club (11160 River Valley Rd NW, Edmonton, AB T5J 2G7)<br><br><img id="myImg" src="https://lh3.ggpht.com/p/AF1QipNg2j_QXBAkb293oMVMYCKK7hnjxIV2KoacEXZ-=s1536" onclick="openModal();currentSlide(3)">')
+    .bindTooltip("GLENORA EZIO/FARAONE");
+
+L.marker([53.501849, -113.559412], {icon: redMarker6}).addTo(map)
+    .bindPopup('<h6>GRANDVIEW WHITEMUD PARK</h6><b>Stair Count:</b> 242-261<br><b>Lat, Long:</b> 53.501849, -113.559412<br><b>Parking Lot:</b> Alfred H. Savage Centre (13909 Fox Dr NW, Edmonton, AB T6H 4P3)<br><br><img id="myImg" src="https://lh3.ggpht.com/p/AF1QipMueO4d4TXGYfSOwsd6SVKSoQkeeTQ_9RuWXHww=s1536" onclick="openModal();currentSlide(4)">')
+    .bindTooltip("GRANDVIEW WHITEMUD PARK");
+
+L.marker([53.508143, -113.566463], {icon: redMarker7}).addTo(map)
+    .bindPopup('<h6>LAURIER DRIVE</h6><b>Stair Count:</b> 165<br><b>Lat, Long #1:</b> 53.508143, -113.566463<br><b>Lat, Long #2: </b> 53.508232, -113.563099<br><b>Street parking:</b> 121 Laurier Dr NW, Edmonton, Alberta<br><b>Notes:</b> Two separate staircases with same entrance from street parking.')
+    .bindTooltip("LAURIER DRIVE");
+
+L.marker([53.499416, -113.592445], {icon: redMarker8}).addTo(map)
+    .bindPopup('<h6>WOLF WILLOW</h6><b>Stair Count:</b> 200-201<br><b>Lat, Long:</b> 53.499416, -113.592445<br><b>Street Parking:</b> 162 Wolf Willow Cres, Edmonton, Alberta<br><br><img id="myImg" src="https://lh3.ggpht.com/p/AF1QipNII55xNMEPfgUTKqD4pTAX5cmiizsyMlXJATFm=s1536" onclick="openModal();currentSlide(5)">')
+    .bindTooltip("WOLF WILLOW");
 
 // Modal
 
